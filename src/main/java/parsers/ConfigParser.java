d6145488fc50eb2ab09cf127f848d4527aec12fa
Vasiliy <devasidmi@gmail.com>
2018-03-26 22:07:52 +0300

    port fix

--- src/main/java/parsers/ConfigParser.java
+++ src/main/java/parsers/ConfigParser.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 
 public class ConfigParser {
 
-    private static String PORT = "port";
+    private static String PORT = "80";
     private static String THREAD_LIMIT = "thread_limit";
     private static String DOCUMENT_ROOT = "document_root";
 
@@ -20,11 +20,11 @@ public class ConfigParser {
             InputStream mInputStream = new FileInputStream(mFile);
             mConfigProperties.load(mInputStream);
 
-            String port = mConfigProperties.getProperty(PORT).split("#")[0].trim();
+//            String port = mConfigProperties.getProperty(PORT).split("#")[0].trim();
             String thread_limit = mConfigProperties.getProperty(THREAD_LIMIT).split("#")[0].trim();
             String document_root = mConfigProperties.getProperty(DOCUMENT_ROOT).split("#")[0].trim();
 
-            Config mConfig = new Config(port, thread_limit, document_root);
+            Config mConfig = new Config(PORT, thread_limit, document_root);
 
             mInputStream.close();
             return mConfig;
